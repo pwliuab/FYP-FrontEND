d715b34cc40c1cf80175a2128f07de45f612399d
lvvv-318 <72491187+lvvv-318@users.noreply.github.com>
2022-01-11 16:41:38 +0800

    Add files via upload

--- src/components/startPage.js
+++ src/components/startPage.js
@@ -18,39 +18,41 @@ function Start(){
             </li>
         </ul>
       </div>
-      <div style={{position:'relative',left:1000, top: 180}}>
-        <img src={CircleYellowSVG} style={{ height: 630, width: 720 }} alt="CircleSVG" />
+      <div style={{position:'relative',left:950, top: 120}}>
+        <img src={CircleYellowSVG} style={{ height: 595, width: 680 }} alt="CircleSVG" />
       </div>
-      <div style={{position:'relative', left:300, top: -450, fontWeight: '600'}}>
+      <div style={{position:'relative', left:210, top: -450, fontWeight: '620'}}>
         <span style={{fontSize:80}}>
           Match Resume
         </span>
       </div>
-      <div style={{position:'relative', left:300, top: -450, fontWeight: '600'}}>
+      <div style={{position:'relative', left:210, top: -450, fontWeight: '620'}}>
         <span style={{fontSize:80}}>
           With Job
         </span>
       </div>
-      <div style={{position:'relative', left:300, top: -450, fontWeight: '600'}}>
+      <div style={{position:'relative', left:210, top: -450, fontWeight: '620'}}>
         <span style={{fontSize:80}}>
           Descriptions
         </span>
       </div>
-      <div style={{position:'relative', left:300, top: -450, fontWeight: '600'}}>
+      <div style={{position:'relative', left:210, top: -420, fontWeight: '600'}}>
         <span style={{fontSize:20}}>
           Get Yourself More job interviews, And get Access to the match Result
         </span>
       </div>
-      <div style={{position:'relative', left:300, top: -450, fontWeight: '600'}}>
+      <div style={{position:'relative', left:210, top: -420, fontWeight: '600'}}>
         <span style={{fontSize:20}}>
           of Resume and Job Description. Help with your Resume Rank and Score More.
+        </span>
+      </div>
+      <div style={{position:'relative', left:210, top: -400, fontWeight: '600'}}>
+        <span style={{fontSize:20, fontStyle:'italic', textDecoration:'underline'}}><a href="#loginPage">
+          Start your journey now.</a>
         </span>
       </div>
     </div>
   );
 }
-
-
-
 
 export default Start;
