07146d92d67cac76e938e56be28709a742e7fb5c
lvvv-318 <72491187+lvvv-318@users.noreply.github.com>
2022-01-11 16:40:54 +0800

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ import Navbar from './components/testing';
 import Login from './components/Login';
 import LoginPage from './components/LoginPage';
 import StartPage from './components/startPage';
-// import SeekerCenterPage from './component/SeekerCenterPage';
-// import CommunityPage from './component/CommunityPage';
+import InputPage from './components/inputPage';
+import CompanyPage from './components/companyPage';
+import SeekerCenterPage from './components/SeekerCenterPage';
+import CommunityPage from './components/CommunityPage';
 const rootPath = process.env.PUBLIC_URL;
 const routes = [
 
@@ -30,19 +32,31 @@ const routes = [
       component: StartPage,
       exact: true,
       breadcrumbName: 'StartPage'
-    },
-  // {
-  //   path:`${rootPath}/SeekerCenterPage`,
-  //   component: SeekerCenterPage,
-  //   exact: true,
-  //   breadcrumbName: 'SeekerCenterPage'
-  // },
-  // {
-  //   path:`${rootPath}/CommunityPage`,
-  //   component: CommunityPage,
-  //   exact: true,
-  //   breadcrumbName: 'CommunityPage'
-  // },
+  },
+  {
+      path:`${rootPath}/inputPage`,
+      component: InputPage,
+      exact: true,
+      breadcrumbName: 'InputPage'
+  },
+  {
+      path:`${rootPath}/companyPage`,
+      component: CompanyPage,
+      exact: true,
+      breadcrumbName: 'CompanyPage'
+  },
+  {
+    path:`${rootPath}/seekerCenterPage`,
+    component: SeekerCenterPage,
+    exact: true,
+    breadcrumbName: 'SeekerCenterPage'
+  },
+  {
+    path:`${rootPath}/communityPage`,
+    component: CommunityPage,
+    exact: true,
+    breadcrumbName: 'CommunityPage'
+  },
 ];
 
 export default routes;
